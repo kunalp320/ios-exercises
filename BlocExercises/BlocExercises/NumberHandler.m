52bc778a00d97e832d0ea65616e89b19e54ed4fc
Kunal Patel <kpatel@Kunals-MacBook-Pro.local>
2016-03-05 15:02:33 -0800

    Made tests pass

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,18 +11,26 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    int numberValue = [number intValue] * 2;
+    NSNumber *numberToReturn =  [[NSNumber alloc] initWithInt:numberValue];
+    return numberToReturn;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+    NSMutableArray *arrayToReturn = [NSMutableArray new];
+    for (NSInteger start = MIN(number, otherNumber); start <= MAX(number, otherNumber); start++) {
+        [arrayToReturn addObject:[[NSNumber alloc] initWithInteger:start]];
+    }
+    return arrayToReturn;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+    id obj = arrayOfNumbers[0];
+    NSInteger min = [obj integerValue];
+    for (NSNumber *currentNumber in arrayOfNumbers) {
+        min = MIN([currentNumber integerValue], min);
+    }
+    return min;
 }
 
 @end
